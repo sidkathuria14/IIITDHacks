575be365ecbac19c849001cdcfc149be5cfa2ecd
Gaurang Goel <gauranggoel.9@gmail.com>
2018-08-17 22:45:21 +0530

    App

--- app/src/main/java/com/example/sidkathuria14/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/sidkathuria14/myapplication/Main2Activity.java
@@ -32,7 +32,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         signInButton = (SignInButton) findViewById(R.id.signIn);
 
