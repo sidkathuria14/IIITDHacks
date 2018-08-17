46f7434e29c7f92bf58500602dff275f1bb7853b
siddharth kathuria <kathuria.siddharth@yahoo.in>
2018-08-17 21:35:39 +0530

    changes made

---
+++ app/src/main/java/com/example/sidkathuria14/myapplication/Main2Activity.java
@@ -0,0 +1,150 @@
+package com.example.sidkathuria14.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class Main2Activity extends AppCompatActivity {
+
+    SignInButton signInButton;
+    GoogleApiClient googleApiClient;
+    FirebaseAuth firebaseAuth=null;
+    FirebaseAuth.AuthStateListener authStateListener;
+    Intent intent1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signInButton = (SignInButton) findViewById(R.id.signIn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+
+        intent1 = new Intent(Main2Activity.this,Main2Activity.class);
+
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this ,new GoogleApiClient.OnConnectionFailedListener(){
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(Main2Activity.this, "connection failed please try again", Toast.LENGTH_SHORT).show();
+            }
+        }).addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions).build();
+
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,101);
+            }
+        });
+
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        /*        FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null)
+                {
+                    Toast.makeText(MainActivity.this, ""+2 , Toast.LENGTH_SHORT).show();
+                    //Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                    //startActivity(intent);
+                   // finish();
+                }
+        */    }
+        };
+
+
+
+        firebaseAuth.addAuthStateListener(authStateListener);
+
+
+        if(authStateListener !=null)
+        {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if(user!=null) {
+                Toast.makeText(Main2Activity.this, ""+3 , Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(Main2Activity.this, Main2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==101)
+        {
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(googleSignInResult.isSuccess())
+            {
+                GoogleSignInAccount account = googleSignInResult.getSignInAccount();
+                int p=firebaseAuthWithGoogle(account);
+                if(p==0)
+                {
+                    Toast.makeText(this, ""+1, Toast.LENGTH_SHORT).show();
+                    startActivity(intent1);
+                    finish();
+                }
+            }
+        }
+    }
+    private int firebaseAuthWithGoogle(GoogleSignInAccount account){
+
+        final int[] k = new int[1];
+        AuthCredential credentials = GoogleAuthProvider.getCredential(account.getIdToken(),null );
+
+        firebaseAuth.signInWithCredential(credentials).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful())
+                {
+                    Toast.makeText(Main2Activity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    k[0] = 1;
+                }
+            }
+        });
+        return k[0];
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+//        firebaseAuth.addAuthStateListener(authStateListener);
+
+    }
+
+    public GoogleApiClient getGoogleApiClient(){
+
+        return googleApiClient;
+    }
+    public FirebaseAuth.AuthStateListener authStateListener(){
+
+        return authStateListener;
+    }
+}
--- app/src/main/java/com/example/sidkathuria14/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sidkathuria14/myapplication/MainActivity.java
@@ -1,17 +1,237 @@
 package com.example.sidkathuria14.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+    public static final String TAG = "main";
+    public static final int rcCC = 33;
+    boolean isCC = false;
+    public static final int REQUEST_CODE_CREATOR = 123;
+String path = "";
+    private GoogleApiClient mGoogleApiClient;
+    private Bitmap mBitmapToSave;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        final Bundle b= getIntent().getExtras();
+//        final TextView textView= (TextView) findViewById(R.id.textView);
+//        final TextView textView2= (TextView) findViewById(R.id.textView2);
+//        Button btn = (Button) findViewById(R.id.button2);
+
+        authStateListener = new Main2Activity().authStateListener();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user!=null) {
+            String k = user.getDisplayName().toString();
+            String k1= user.getEmail().toString();
+            Toast.makeText(MainActivity.this, k+"\n"+k1, Toast.LENGTH_SHORT).show();
+//            textView.setText(k);
+//            textView2.setText(k1);
+            Log.d(TAG, "onCreate: " + k);
+            Log.d(TAG, "onCreate: " + k1);
+        }
 
 
+        ((Button)findViewById(R.id.logout)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                firebaseAuth.removeAuthStateListener(authStateListener);
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
 
+        ((Button)findViewById(R.id.choose)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 200);
+            }
+        });
+        Log.d(TAG, "onCreate: " + path);
+
+//        Intent intent1 = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(intent1, 0);
+
+//        File fileMetadata = new File();
+//        fileMetadata.setName("photo.jpg");
+//        java.io.File filePath = new java.io.File("files/photo.jpg");
+//        FileContent mediaContent = new FileContent("image/jpeg", filePath);
+//        File file = driveService.files().create(fileMetadata, mediaContent)
+//                .setFields("id")
+//                .execute();
+
+
+    }
+//    public String getPathFromURI(Uri contentUri) {
+//        String res = null;
+//        String[] proj = {MediaStore.Images.Media.DATA};
+//        Cursor cursor = this.getContentResolver().query(contentUri, proj, "", null, "");
+//        if (cursor.moveToFirst()) {
+//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+//            res = cursor.getString(column_index);
+//        }
+//        cursor.close();
+//        return res;
+//    }
+private void saveFileToDrive() {
+
+    final Bitmap image = mBitmapToSave;
+    Drive.DriveApi.newDriveContents(mGoogleApiClient)
+            .setResultCallback(new ResultCallback<DriveApi.DriveContentsResult>() {
+
+                @Override
+                public void onResult(DriveApi.DriveContentsResult result) {
+
+                    if (!result.getStatus().isSuccess()) {
+                        Log.i("ERROR", "Failed to create new contents.");
+                        return;
+                    }
+
+
+                    OutputStream outputStream = result.getDriveContents().getOutputStream();
+                    // Write the bitmap data from it.
+                    ByteArrayOutputStream bitmapStream = new ByteArrayOutputStream();
+                    image.compress(Bitmap.CompressFormat.PNG, 100, bitmapStream);
+                    try {
+                        outputStream.write(bitmapStream.toByteArray());
+                    } catch (IOException e1) {
+                        Log.i("ERROR", "Unable to write file contents.");
+                    }
+                    // Create the initial metadata - MIME type and title.
+                    // Note that the user will be able to change the title later.
+                    MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+                            .setMimeType("image/jpeg").setTitle("Android Photo.png").build();
+                    // Create an intent for the file chooser, and start it.
+                    IntentSender intentSender = Drive.DriveApi
+                            .newCreateFileActivityBuilder()
+                            .setInitialMetadata(metadataChangeSet)
+                            .setInitialDriveContents(result.getDriveContents())
+                            .build(mGoogleApiClient);
+                    try {
+                        startIntentSenderForResult(
+                                intentSender, REQUEST_CODE_CREATOR, null, 0, 0, 0);
+                    } catch (IntentSender.SendIntentException e) {
+                        Log.i("ERROR", "Failed to launch file chooser.");
+                    }
+                }
+            });
+}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
+            // Create the API client and bind it to an instance variable.
+            // We use this instance as the callback for connection and connection
+            // failures.
+            // Since no account name is passed, the user is prompted to choose.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(MainActivity.this)
+                    .addOnConnectionFailedListener(MainActivity.this)
+                    .build();
+        }
+        // Connect the client. Once connected, the camera is launched.
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        if (resultCode == this.RESULT_OK) {
+            if (requestCode == 200) {
+                Uri selectedImageUri = imageReturnedIntent.getData();
+                if (null != selectedImageUri) {
+                    path = getPathFromURI(selectedImageUri);
+                    Log.d(TAG, "onActivityResult: " + path);
+//                    return path;
+//                    DrawerAdapter.imageViewPP.setImageURI(selectedImageUri);
+                }
+            }else if(requestCode==0){
+                Uri selectedImageUri = imageReturnedIntent.getData();
+                if (null != selectedImageUri) {
+                    path = getPathFromURI(selectedImageUri);
+                    //DrawerAdapter.imageViewPP.setImageURI(selectedImageUri);
+                    Log.d(TAG, "onActivityResult: " + path);
+
+//                    return path;
+                }
+            }
+        }
+    }
+
+    public String getPathFromURI(Uri contentUri) {
+        String res = null;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = this.getContentResolver().query(contentUri, proj, "", null, "");
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected: ");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended: ");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: ");
     }
 }
